380d33acded2bd131b00839df41e925dccecbe81
Dawn inShadow <dawninshadow0.0@gmail.com>
2017-09-13 15:28:05 +0800

    Vue!!!

---
+++ firstpoj/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1 v-html="zpy"></h1>
+    <ul>
+        <li v-for="item in items" v-bind:class="{redclass:item.sex,xxx}">
+            {{item.name}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+      Hello
+  },
+  data:function(){
+      return{
+          zpy:'<h1>textzpy</h1>',
+          items:[
+              {
+                  name:'tassadar',
+                  sex:true
+
+              },
+              {
+                  name:'kanruige',
+                  sex:false
+
+              }
+          ],
+          xxx:'blueclass',
+      }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.redclass{
+    color: #e91e63;
+}
+.blueclass{
+    color: #2196f3;
+}
+</style>
