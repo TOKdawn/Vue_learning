1a17356eda5fcc85509dc30210306b8daa0a0562
Dawn inShadow <dawninshadow0.0@gmail.com>
2017-09-29 20:08:00 +0800

    学习Vue

---
+++ JE预备/JEdemo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <div class = "header">
+     I am header
+   </div>
+   <div class = "content">
+     我是内容
+   </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
